e19ed11c90b02a861064b2dd73e4ed8c0d58eb39
shangcom <dndbs91@gmail.com>
2024-09-07 17:28:01 +0900

    스프링 MVC 1편 섹션 7 스프링 MVC- 기본 기능 요청 매핑 - API 예시 완료

--- src/main/java/hello/springmvc/basic/requestmapping/MappingClassController.java
+++ src/main/java/hello/springmvc/basic/requestmapping/MappingClassController.java
@@ -3,6 +3,7 @@ package hello.springmvc.basic.requestmapping;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/mapping/users")
 public class MappingClassController {
 
     /**
@@ -11,29 +12,31 @@ public class MappingClassController {
      * 회원 조회: GET /users/{userId}
      * 회원 수정: PATCH /users/{userId}
      * 회원 삭제: DELETE /users/{userId}
+     * "/mapping/users" 까지 겹치니까 그냥 클래스 단위에서 @RequestMapping("/mapping/users")로 빼내고
+     * 각 컨트롤러 메서드에는 중복되지 않는 경로만 써주면 된다.
      */
 
-    @GetMapping("/mapping/users")
+    @GetMapping
     public String user() {
         return "get users";
     }
 
-    @PostMapping("/mapping/users")
+    @PostMapping
     public String addUser() {
         return "post user";
     }
 
-    @GetMapping("/mapping/users/{userId}")
+    @GetMapping("/{userId}")
     public String findUser(@PathVariable String userId) {
         return "get userId= " + userId;
     }
 
-    @PatchMapping("/mapping/users/{userId}")
+    @PatchMapping("/{userId}")
     public String updateUser(@PathVariable String userId) {
         return "update userId= " + userId;
     }
 
-    @DeleteMapping("/mapping/users/{userId}")
+    @DeleteMapping("/{userId}")
     public String deleteUser(@PathVariable String userId) {
         return "delete userId= " + userId;
     }
